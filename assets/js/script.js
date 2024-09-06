023f20da1d53486c355209d83f79db3e0a6f501d
Sophie Dufrane <dufranesophie@gmail.com>
2024-09-06 16:00:07 +0000

    Add event listeners to the toggle button and restart button. Work on function to toggle the visibility of the rules section when clicking on the button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,31 @@
 // Wait for the DOM to finish loading before running the game
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByClassName("icon-option");
    
     for (let button of buttons) {
-        button.addEventListener("click", function() {
-        let userSelection = this.getAttribute("data-selection"); // Add event listener to get attribute of the icon selected
+        button.addEventListener("click", function() { // Add event listener to get attribute of the icon selected
+        let userSelection = this.getAttribute("data-selection");
         console.log(userSelection);
         })
     }
 }
-
 );
 
-function toggleRulesSection() {
+// Add event listeners to to the toggle button and the restart button
+document.getElementById("toggle-button").addEventListener("click", toggleRulesSection);
+document.getElementById("restart-button").addEventListener("click", restartGame);
 
+
+// Function to toggle the visibility of the rules section when clicking on the button
+function toggleRulesSection() {
+    let toggleButton = document.getElementById("toggle-button");
+    let rulesSection = document.getElementById("rules-section")
+
+    if (rulesSection.style.display === "none") {
+        rulesSection.style.display = "block"
+    } else {
+        rulesSection.style.display = "none"
+    }
 }
 
 function runGame() {
@@ -41,5 +53,5 @@ function updateAndIncrementScore() {
 }
 
 function restartGame() {
-
+    console.log("Restart is clicked!")
 }
