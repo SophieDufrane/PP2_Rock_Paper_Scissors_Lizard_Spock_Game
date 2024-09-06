8f071dd08b26de86d19c1b0c41471870bae17fca
Sophie Dufrane <dufranesophie@gmail.com>
2024-09-06 14:42:22 +0000

    Add main function to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("connected!")
+function toggleRulesSection() {
+
+}
+
+function runGame() {
+
+}
+
+function checkWinner() {
+    
+}
+
+function updateSelectedIcon() {
+
+}
+
+function udpateRuleApplied() {
+
+}
+
+function updateRoundNumber() {
+    
+}
+
+function updateAndIncrementScore() {
+
+}
+
+function restartGame() {
+
+}
