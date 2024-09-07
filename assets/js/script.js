45be2961fd93db8cc8be34d4e215264da63d416f
Sophie Dufrane <dufranesophie@gmail.com>
2024-09-07 14:41:00 +0000

    Create an object to defining the winning rules, based on the combinations of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,17 +23,33 @@ document.getElementById("restart-button").addEventListener("click", restartGame)
 // Define an array containing the choices
 let choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
+// Object to define the winning rules based on possible combinations
+const rulesConditions = {
+    rock: ["scissors","lizard"],
+    paper: ["rock","spock"],
+    scissors: ["paper","lizard"],
+    lizard: ["paper","spock"],
+    spock: ["scissors","rock"],
+};
+
+// Main function of the game that take the user selection as a parameter, create the computer's one with random number, then call functions to update the icons and check who win the round
 function runGame(userSelection) {
     // Define the computer selection with random number
     let computerSelection = choices[Math.floor(Math.random() * choices.length)];
+
+     // Call the function updateSelectedIcon with user and computer selection
     updateSelectedIcon(userSelection, computerSelection);
 
+    // Call the function checkWinner with user and computer selection
+    checkWinner(userSelection, computerSelection);
+
 }
 
-function checkWinner() {
+function checkWinner(userSelection, computerSelection) {
 
 }
 
+// Update the icons in result section to reflect the user's and the computer's selection
 function updateSelectedIcon(userSelection, computerSelection) {
     let userImage = document.getElementById("user-selection");
     userImage.src = `assets/images/${userSelection}.png`;
