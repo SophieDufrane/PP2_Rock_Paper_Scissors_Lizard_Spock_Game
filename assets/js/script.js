188b64b2ae01c883fa6ddbbfe30cbd05072e594e
Sophie Dufrane <dufranesophie@gmail.com>
2024-09-07 15:31:47 +0000

    Create a function that updates the message in the result section, based on the outcome of the round (and the checkWinner result)

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ document.getElementById("restart-button").addEventListener("click", restartGame)
 let choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
 // Object to define the winning rules based on possible combinations
-const rulesConditions = {
+const rules = {
     rock: ["scissors", "lizard"],
     paper: ["rock", "spock"],
     scissors: ["paper", "lizard"],
@@ -35,24 +35,13 @@ const rulesConditions = {
 // Main function of the game that take the user selection as a parameter, create the computer's one with random number, then call functions to update the icons and check who win the round
 function runGame(userSelection) {
     // Define the computer selection with random number
-    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    let computerSelection = choices[Math.floor(Math.random()*choices.length)];
 
     // Call the function updateSelectedIcon with user and computer selection
     updateSelectedIcon(userSelection, computerSelection);
 
     // Call the function checkWinner with user and computer selection
     checkWinner(userSelection, computerSelection);
-}
-
-// Function that checks who is the winner, passes 2 arguments: the user and computer selection, then verifies within the rulesConditions wich scenario applies
-function checkWinner(userSelection, computerSelection) {
-    if (userSelection === computerSelection) {
-        console.log("It's a draw");
-    } else if (rulesConditions[userSelection].includes(computerSelection)) {
-        console.log("User wins!");
-    } else {
-        console.log("Computer wins!");
-    }
 }
 
 // Update the icons in result section to reflect the user's and the computer's selection
@@ -66,9 +55,32 @@ function updateSelectedIcon(userSelection, computerSelection) {
     computerImage.alt = `${computerSelection} image`;
 }
 
-function udpateRuleApplied() {
+// Function that checks who is the winner, passes 2 arguments: the user and computer selection, then verifies within the rulesConditions wich scenario applies
+function checkWinner(userSelection, computerSelection) {
+    let result;
+    
+    if (userSelection === computerSelection) {
+        result = "draw";
+    } else if (rules[userSelection].includes(computerSelection)) {
+        result = "win";
+    } else {
+        result = "lose";
+    }
 
+    udpateRuleApplied(result);
 }
+
+function udpateRuleApplied(result) {
+    let resultMessage = document.getElementById("rule-applied");
+    
+    if (result === "draw") {
+        resultMessage.textContent = "Mind match, draw!";
+    } else if (result === "win") {
+        resultMessage.textContent = "You Win!";
+    } else {
+        resultMessage.textContent = "Kirk beat you!";
+    }
+}   
 
 function updateRoundNumber() {
 
