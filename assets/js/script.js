dff7972677cbd06f36ceb8a1846ed17035855e6d
Sophie Dufrane <dufranesophie@gmail.com>
2024-09-07 18:11:44 +0000

    Update the runGame function to store the result from checkWinner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,8 @@
-// Wait for the DOM to finish loading before running the game
-document.addEventListener("DOMContentLoaded", function setUpGame() {
-
-    let icons = document.getElementsByClassName("icon-option");
-
-    for (let icon of icons) {
-        icon.addEventListener("click", function iconClicked() { // Add event listener to get attribute of the icon selected
-
-            let userSelection = this.getAttribute("data-selection");
-            console.log("The user selection is", userSelection);
-
-            // Call the rungame function
-            runGame(userSelection);
-        })
-    }
-}
-);
-
-// Add event listeners to the toggle button and the restart button
-document.getElementById("toggle-button").addEventListener("click", toggleRulesSection);
-document.getElementById("restart-button").addEventListener("click", restartGame);
+// Define a maximum points to end the game
+const maxPoints = 5;
 
 // Define an array containing the choices
-let choices = ["rock", "paper", "scissors", "lizard", "spock"];
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
 // Object to define the winning rules based on possible combinations
 const rules = {
@@ -35,17 +16,35 @@ const rules = {
 // Initialise a variable to keep track of the round number
 let roundNumber = 1;
 
+// Wait for the DOM to finish loading before running the game
+document.addEventListener("DOMContentLoaded", function setUpGame() {
+
+    let icons = document.getElementsByClassName("icon-option");
+
+    for (let icon of icons) {
+        icon.addEventListener("click", function iconClicked() { // Add event listener to get attribute of the icon selected
+
+            let userSelection = this.getAttribute("data-selection");
+
+            // Call the rungame function
+            runGame(userSelection);
+        })
+    }
+}
+);
+
+// Event listeners for toggle and restart button
+document.getElementById("toggle-button").addEventListener("click", toggleRulesSection);
+document.getElementById("restart-button").addEventListener("click", restartGame);
+
 // Main function of the game that take the user selection as a parameter, create the computer's one with random number, then call functions to update the icons and check who win the round
 function runGame(userSelection) {
     // Define the computer selection with random number
     let computerSelection = choices[Math.floor(Math.random()*choices.length)];
-
-    // Call the function updateSelectedIcon with user and computer selection
     updateSelectedIcon(userSelection, computerSelection);
 
-    // Call the function checkWinner with user and computer selection
-    checkWinner(userSelection, computerSelection);
-
+    let result = checkWinner(userSelection, computerSelection);
+    udpateRuleApplied();
     updateGameStatus();
 }
 
@@ -61,18 +60,14 @@ function updateSelectedIcon(userSelection, computerSelection) {
 }
 
 // Function that checks who is the winner, passes 2 arguments: the user and computer selection, then verifies within the rulesConditions wich scenario applies
-function checkWinner(userSelection, computerSelection) {
-    let result;
-    
+function checkWinner(userSelection, computerSelection) {    
     if (userSelection === computerSelection) {
-        result = "draw";
+        return "draw";
     } else if (rules[userSelection].includes(computerSelection)) {
-        result = "win";
+        return "win";
     } else {
-        result = "lose";
+        return "lose";
     }
-
-    udpateRuleApplied(result);
 }
 
 // Function that updates the message in the result section, based on the outcome of the round (and the checkWinner result) 
@@ -92,7 +87,6 @@ function udpateRuleApplied(result) {
  * Update the game status and display round number
  */
 function updateGameStatus() {
-    
     let gameStatus = document.getElementById("game-status-message");
     gameStatus.textContent = `Round ${roundNumber}`;
     roundNumber++;
